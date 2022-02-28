24764f89d98d9bc030b9b83dd940f7dc9b9b2e9e
Omar <o.moreta@gmail.com>
2022-02-28 17:40:31 -0500

    added coin component to display

--- App.js
+++ App.js
@@ -1,11 +1,45 @@
+import React from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Crypto Tracker</Text>
-      <StatusBar style="auto" />
+      <View style={styles.coinContainer}>
+        <Image
+          source={{
+            uri: "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579",
+          }}
+          style={{
+            height: 30,
+            width: 30,
+            marginRight: 10,
+            alignSelf: "center",
+          }}
+        />
+        <View>
+          <Text style={styles.title}>Bitcoin</Text>
+          <View style={{ flexDirection: "row" }}>
+            <View style={styles.rankContainer}>
+              <Text style={styles.rank}>1</Text>
+            </View>
+            <Text style={styles.text}>BTC</Text>
+            <AntDesign
+              name="caretdown"
+              size={12}
+              color="white"
+              style={{ alignSelf: "center", marginRight: 5 }}
+            />
+            <Text style={styles.text}>0.63%</Text>
+          </View>
+        </View>
+        <View style={{ marginLeft: "auto" }}>
+          <Text style={styles.title}>56265.09</Text>
+          <Text style={styles.text}>MCap 1.076 T</Text>
+        </View>
+      </View>
+      <StatusBar style="light" />
     </View>
   );
 }
@@ -13,8 +47,33 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "#121212",
+    paddingTop: 50,
+  },
+  title: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
+    marginBottom: 3,
+  },
+  text: {
+    color: "white",
+    marginRight: 5,
+  },
+  coinContainer: {
+    flexDirection: "row",
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderBottomColor: "white",
+    padding: 15,
+  },
+  rankContainer: {
+    backgroundColor: "#585858",
+    borderRadius: 5,
+    paddingHorizontal: 6,
+    marginRight: 5,
+  },
+  rank: {
+    fontWeight: "bold",
+    color: "white",
   },
 });
